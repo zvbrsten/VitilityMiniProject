be301bfde3a499b6df436a2c4979fad6089522bf
sinharitamvaraa <161631438+sinharitamvaraa@users.noreply.github.com>
2024-04-22 19:42:57 +0530

    Update script.js

--- components/gpaneeded/script.js
+++ components/gpaneeded/script.js
@@ -1,10 +1,9 @@
 function calculateCGPA() {
-    var CGPAold = parseFloat(document.getElementById("CGPAold").value); // CGPA till previous semester 
-    var CreditsOld = parseFloat(document.getElementById("CreditsOld").value); // Credits till previous semester
-    var DesiredCGPA = parseFloat(document.getElementById("DesiredCGPA").value); // Desired CGPA
-    var CreditsTaken = parseFloat(document.getElementById("CreditsTaken").value); // Credits taken this semester
+    var CGPAold = parseFloat(document.getElementById("CGPAold").value); 
+    var CreditsOld = parseFloat(document.getElementById("CreditsOld").value); 
+    var DesiredCGPA = parseFloat(document.getElementById("DesiredCGPA").value); 
+    var CreditsTaken = parseFloat(document.getElementById("CreditsTaken").value); 
 
-    // Calculate the required GPA in the current semester to achieve the desired CGPA
     var RequiredGPA = (CreditsOld*(DesiredCGPA-CGPAold)+DesiredCGPA*CreditsTaken)/CreditsTaken;
 
     document.getElementById("RequiredGPA").textContent = RequiredGPA.toFixed(2);
